e4dd90c2916f1afc1b244fd410136c317152c427
MeraDicycle <1376541252@qq.com>
2025-11-27 15:45:53 +0800

    Delete front directory

--- front/src/views/CommunityView.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <Nav></Nav>
-</template>
-
-<style></style>
-
-<script setup>
-  import Nav from '@/components/community/Nav.vue'
-</script>
--- front/src/views/Video.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <Nav></Nav>
-</template>
-
-<style></style>
-
-<script setup>
-  import Nav from '@/components/video/Nav.vue'
-</script>
