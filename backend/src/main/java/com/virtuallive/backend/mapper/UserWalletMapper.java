6abb7d2ba7d156b41367807f271627bc946f4a89
CLY826 <2916243371@qq.com>
2025-12-17 17:43:25 +0800

    Delete backend/src/main/java/com/virtuallive/backend/mapper directory

--- backend/src/main/java/com/virtuallive/backend/mapper/UserWalletMapper.java
+++
@@ -1,7 +0,0 @@
-package com.virtuallive.backend.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.virtuallive.entity.UserWallet;
-
-public interface UserWalletMapper extends BaseMapper<UserWallet> {
-}
